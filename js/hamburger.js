9db0f1859d0499ee38629467b22258acf169e904
Fujinaka Sho <fujinakasho@FujinakanoMacBook-Air.local>
2026-01-07 14:34:53 +0900

    fix_hambergerMenu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,15 @@
-const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("headerNav");
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.header_nav');
+const navLinks = document.querySelectorAll('.header_nav a');
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("is-open");
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('is-open');
+  hamburger.classList.toggle('is-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('is-open');
+    hamburger.classList.remove('is-open');
+  });
 });
